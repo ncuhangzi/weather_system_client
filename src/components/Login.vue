74bd9c23de20f9b1acd5b74fe257688f53ef5bdb
Hangzi <st30201@gmail.com>
2020-12-29 21:10:29 +0800

    store some change

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,119 +1,134 @@
 <template>
-<div class="loginform">
-    {{test}}
+  <div class="loginform">
+    {{ test }}
 
-    <el-form :rules="rules" ref="ruleForm" label-width="100px" class="demo-ruleForm">
-    <el-form-item label="E-mail" prop="email" label-width="100px">
-        <el-input v-model="username"></el-input>
-    </el-form-item>
-    <el-form-item label="密碼" prop="password">
-        <el-input v-model="password" show-password></el-input>
-    </el-form-item>
-    <el-form-item>
-        <el-button type="text" round @click="to('/register')">建立帳號</el-button>
-        <el-button type="primary" round @click="submit">登入</el-button>
-    </el-form-item>
+    <el-form
+      :rules="rules"
+      :model="ruleForm"
+      ref="ruleForm"
+      label-width="100px"
+      class="demo-ruleForm"
+    >
+      <el-form-item label="E-mail" prop="email" label-width="100px">
+        <el-input v-model="ruleForm.email"></el-input>
+      </el-form-item>
+      <el-form-item label="密碼" prop="password">
+        <el-input v-model="ruleForm.password" show-password></el-input>
+      </el-form-item>
+      <el-form-item>
+        <el-button type="text" round @click="to('/register')"
+          >建立帳號</el-button
+        >
+        <el-button type="primary" round @click="submitForm('ruleForm')"
+          >登入</el-button
+        >
+      </el-form-item>
     </el-form>
-
-</div>
-  
+  </div>
 </template>
 
 <script>
-import axios from 'axios'
-import {API_BASE_URL} from './config.js'
+import axios from "axios";
+import { API_BASE_URL } from "./config.js";
 
 export default {
-    name:'login',
-    data(){
-        return{
-            token: '',
-            username: '',
-            password: '',
-            test: [],
-            rules: {
-                email: [
-                    { required: true, message: '請輸入正確email格式', trigger: 'blur' },
-                    { min:1, max: 30, message: '長度須介於1到30字元', trigger: 'blur' }
-                ],
-                password: [
-                    { required: true, message: '密碼不得留空', trigger: 'change' },
-                    { min:8, max: 15, message: '長度須小於8到15字元', trigger: 'blur' }
-                ]
-            },
+  name: "login",
+  data() {
+    return {
+      token: "",
+      username: "",
+      password: "",
+      test: [],
 
+      ruleForm: {
+        email: "",
+        password: "",
+      },
+      rules: {
+        email: [
+          { required: true, message: "請輸入email", trigger: "blur" },
+          { type: 'email', message: "請輸入正確email格式", trigger: ['blur', 'change'] },
+          { min: 1, max: 30, message: "長度須介於1到30字元", trigger: "blur" },
+        ],
+        password: [
+          { required: true, message: "密碼不得留空", trigger: "change" },
+          { min: 8, max: 15, message: "長度須小於8到15字元", trigger: "blur" },
+        ],
+      },
+    };
+  },
+  mounted() {
+    this.checklogin();
+    this.gettest();
+  },
+  methods: {
+    checklogin() {},
+    submit() {
+      axios({
+        method: "post",
+        url: API_BASE_URL + "/login",
+        headers: {
+          username: this.username,
+          password: this.password,
+        },
+        data: {},
+      })
+        .catch(function (error) {
+          // alert(error)
+          console.log(error);
+        })
+        .then((res) => {
+          this.token = res.data.token;
+          //write login authencation logic here!
+          if (this.token == "Imlogin") {
+            sessionStorage.setItem("token", "Imlogin");
+            this.$router.push("/");
+          } else {
+            alert("login failed");
+          }
+        });
+    },
+    gettest() {
+      axios({
+        method: "get",
+        url: API_BASE_URL + "/test",
+        headers: {},
+      })
+        .catch(function (error) {
+          // alert(error)
+          console.log(error);
+        })
+        .then((res) => {
+          console.log(res);
+
+          this.test = res.data;
+        });
+    },
+    to(path) {
+      this.opened = false;
+      this.$router.push({ path: path });
+    },
+
+    submitForm(formName) {
+      this.$refs[formName].validate((valid) => {
+        if (valid) {
+          alert("submit!");
+        } else {
+          console.log("error submit!!");
+          return false;
         }
+      });
     },
-    mounted(){
-        this.checklogin();
-        this.gettest();
-    },
-    methods:{
-        checklogin(){
+  },
 
-        },
-        submit(){
-          axios({
-              method: 'post',
-              url: API_BASE_URL + '/login',
-              headers : { 
-                'username' : this.username,
-                'password' : this.password,
-              },
-              data: {
-
-              },
-
-            }).catch(function (error) {
-                          // alert(error)
-                          console.log(error);    
-            }).then((res)=>{
-
-              this.token = res.data.token
-                //write login authencation logic here!
-                if( this.token == 'Imlogin' ){
-                sessionStorage.setItem('token', 'Imlogin')
-                this.$router.push('/');
-                } else{
-                alert('login failed')
-                }           
-            })
-        },
-        gettest(){
-          axios({
-              method: 'get',
-              url: API_BASE_URL + '/test',
-              headers : { 
-
-              },
-
-            }).catch(function (error) {
-                          // alert(error)
-                          console.log(error);    
-            }).then((res)=>{
-                console.log(res)
-
-              this.test = res.data
-                           
-            })
-        },
-        to(path) {
-            this.opened = false;
-            this.$router.push({path: path});
-        },
-    }
-
-
-
-
-//send:[post] username, password
-//recieve: token, login result
-}
+  //send:[post] username, password
+  //recieve: token, login result
+};
 </script>
 
 <style>
-.loginform{
-    width: 500px;
-    margin:0px auto;
+.loginform {
+  width: 500px;
+  margin: 0px auto;
 }
 </style>
